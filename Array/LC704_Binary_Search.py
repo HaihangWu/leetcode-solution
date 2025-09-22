04564455704d0788e30f18e4afae0e9df32c9d0f
hthh1211 <hthh1211@sina.com>
2025-09-22 11:12:02 +0800

    Merge commit '8738d0b11d6e34886fc7912c7441c1b63c2e9657' as 'VGGT'

--- Array/LC704_Binary_Search.py
+++ Array/LC704_Binary_Search.py
@@ -1,25 +1,20 @@
 class Solution(object):
-    def search(self, nums, target):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
 
-        searching_starting_index = 0
-        searching_end_index = len(nums) - 1
-        if target > nums[searching_end_index] or target < nums[searching_starting_index]:
-            return -1
+        while left <= right:
+            while left < right and nums[right] == val:
+                right -= 1
 
-        while searching_starting_index <= searching_end_index:
+            if nums[left] == val:
+                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1
 
-            searching_middle_index = (searching_starting_index + searching_end_index) // 2
+            left += 1
 
-            if target == nums[searching_end_index]:
-                return searching_end_index
-            elif target == nums[searching_starting_index]:
-                return searching_starting_index
-            elif target == nums[searching_middle_index]:
-                return searching_middle_index
-            elif (searching_end_index - searching_starting_index) == 1:
-                return -1
-
-            if target > nums[searching_middle_index]:
-                searching_starting_index = searching_middle_index
-            elif target < nums[searching_middle_index]:
-                searching_end_index = searching_middle_index
+        return right + 1
