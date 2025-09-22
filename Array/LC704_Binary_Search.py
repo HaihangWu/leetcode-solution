81390bbb4eb651379be6ac13b464d611eb955d13
hthh1211 <hthh1211@sina.com>
2025-09-22 09:40:45 +0800

    Merge commit '8738d0b11d6e34886fc7912c7441c1b63c2e9657' as 'VGGT'

--- Array/LC704_Binary_Search.py
+++ Array/LC704_Binary_Search.py
@@ -1,1 +1,25 @@
-import math
+class Solution(object):
+    def search(self, nums, target):
+
+        searching_starting_index = 0
+        searching_end_index = len(nums) - 1
+        if target > nums[searching_end_index] or target < nums[searching_starting_index]:
+            return -1
+
+        while searching_starting_index <= searching_end_index:
+
+            searching_middle_index = (searching_starting_index + searching_end_index) // 2
+
+            if target == nums[searching_end_index]:
+                return searching_end_index
+            elif target == nums[searching_starting_index]:
+                return searching_starting_index
+            elif target == nums[searching_middle_index]:
+                return searching_middle_index
+            elif (searching_end_index - searching_starting_index) == 1:
+                return -1
+
+            if target > nums[searching_middle_index]:
+                searching_starting_index = searching_middle_index
+            elif target < nums[searching_middle_index]:
+                searching_end_index = searching_middle_index
