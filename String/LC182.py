67aeea8496f6a4f4767387f359bdd56145f871a5
hthh1211 <hthh1211@sina.com>
2025-09-27 13:29:54 +0800

    Merge remote-tracking branch 'origin/main'

--- String/LC182.py
+++ String/LC182.py
@@ -1,45 +1,26 @@
 class Solution(object):
-    def reverseWords(self, s):
+    def swap(self, left,right,password):
+        while left<right:
+            password[left], password[right]=password[right], password[left]
+            left+=1
+            right-=1
+        return password
+
+    def dynamicPassword(self, password, target):
         """
-        :type s: str
+        :type password: str
+        :type target: int
         :rtype: str
         """
-        s = list(s)
-        n = len(s)
-        slow = 0
-        fast = 0
-        word_start = False
-        word_size = 0
-        while fast < n:
-            if s[fast] != " ":
-                if slow == 0:
-                    pass
-                else:
-                    s[slow] = " "
-                    slow += 1
-                while fast < n and s[fast] != " ":
-                    s[slow] = s[fast]
-                    slow += 1
-                    fast += 1
-            fast += 1
-
-        s = s[0:slow]
-        n = len(s)
-        left = 0
-        right = n - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
-
-        word_left = 0
-        word_right = 0
-        for i in range(n):
-            if s[i] == " " or i == n - 1:
-                word_right = i if i == n - 1 else i - 1
-                while word_left < word_right:
-                    s[word_left], s[word_right] = s[word_right], s[word_left]
-                    word_left += 1
-                    word_right -= 1
-                word_left = i + 1
-        return "".join(s)
+        password=list(password)
+        n=len(password)
+        left=0
+        right=target-1
+        password=self.swap(left,right,password)
+        left=target
+        right=n-1
+        password=self.swap(left,right,password)
+        left=0
+        right=n-1
+        password=self.swap(left,right,password)
+        return ''.join(password)
