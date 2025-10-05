99eaef9e4755897e80312c3f97bd3ab6c1a8e03b
hthh1211 <hthh1211@sina.com>
2025-10-05 22:40:49 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ Dynamic Programming/LC343.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        mem = [-1] * (n + 1)
+        mem[1] = 1
+
+        def dfs(k):
+            if mem[k] == -1:
+                best = 0
+                j = 1
+                while j < k:
+                    best = max(best, j * (k - j), dfs(j) * (k - j))
+                    j += 1
+
+                mem[k] = best
+
+            return mem[k]
+
+        print(mem)
+
+        return dfs(n)
